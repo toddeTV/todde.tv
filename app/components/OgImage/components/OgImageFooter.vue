1e9761e3669af6778c76611ce2f741e8bfbd5d70
Thorsten Seyschab <business@todde.tv>
2026-03-07 01:01:39 +0100

    feat: add ogImage for talks, add & refactor ogImage components

--- app/components/OgImage/components/OgImageFooter.vue
+++ app/components/OgImage/components/OgImageFooter.vue
@@ -7,44 +7,53 @@
 </script>
 
 <template>
-  <!-- Author name and site URL -->
-  <div
-    :style="{
-      display: 'flex',
-      flexWrap: 'nowrap',
-      justifyContent: 'space-between',
-      alignItems: 'flex-end',
-    }"
-  >
-    <div
-      :style="{
-        fontSize: '24px',
-        fontWeight: 600,
-        color: '#00dc82',
-      }"
-    >
-      Thorsten Seyschab
-    </div>
-    <div
-      :style="{
-        fontSize: '22px',
-        fontWeight: 400,
-        color: '#71717a',
-      }"
-    >
-      todde.tv
-    </div>
-  </div>
-
-  <!-- Bottom gradient accent line -->
   <div
     :style="{
       position: 'absolute',
-      bottom: '0',
-      left: '0',
-      right: '0',
-      height: '4px',
-      background: 'linear-gradient(90deg, #00dc82, #00c474, transparent)',
+      bottom: '60px',
+      left: '60px',
+      right: '60px',
     }"
-  />
+  >
+    <!-- Author name and site URL -->
+    <div
+      :style="{
+        display: 'flex',
+        flexWrap: 'nowrap',
+        justifyContent: 'space-between',
+        alignItems: 'flex-end',
+      }"
+    >
+      <div
+        :style="{
+          fontSize: '24px',
+          fontWeight: 600,
+          color: '#00dc82',
+        }"
+      >
+        Thorsten Seyschab
+      </div>
+      <div
+        :style="{
+          fontSize: '22px',
+          fontWeight: 400,
+          color: '#71717a',
+        }"
+      >
+        todde.tv
+      </div>
+    </div>
+
+    <!-- Bottom gradient accent line -->
+    <div
+      :style="{
+        position: 'absolute',
+        bottom: '0',
+        left: '0',
+        right: '0',
+        height: '4px',
+        background: 'linear-gradient(90deg, #00dc82, #00c474, transparent)',
+      }"
+    />
+  </div>
 </template>
