873f29fd7dc7b9d4ca84251784ebed5e31280433
Thorsten Seyschab <hello@todde.tv>
2026-05-18 21:36:25 +0200

    fix: align site seo name

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -106,7 +106,7 @@ export default defineNuxtConfig({
   site: { // for `@nuxtjs/seo` - shared site config used by all SEO sub-modules (like `ogImage`, `schemaOrg`, etc.)
     url: projectConfig.siteUrl,
     // name: 'todde.tv',
-    name: projectConfig.author.name,
+    name: projectConfig.projectName,
     description: projectConfig.siteDescription,
     defaultLocale: 'en',
   },
