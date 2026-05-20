174790d22be11546619b3ed53c7bfcb60a302b8e
Thorsten Seyschab <hello@todde.tv>
2026-05-20 15:05:54 +0200

    refactor: simplify legal page empty variables (#44)

--- app/components/content/LegalAuthorName.vue
+++ app/components/content/LegalAuthorName.vue
@@ -7,8 +7,5 @@ const legalName = computed(() => config.public.legalName as string)
 </script>
 
 <template>
-  <span v-if="legalName">{{ legalName }}</span>
-  <span v-else class="text-text-dim italic">
-    [Legal name not configured - set NUXT_PUBLIC_LEGAL_NAME]
-  </span>
+  <span>{{ legalName }}</span>
 </template>
